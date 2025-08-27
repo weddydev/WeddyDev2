d3fb3e7f6a1326b4be72a06b12642ae02bbbb54f
syedtalha88 <talhasyed910@gmail.com>
2025-08-27 14:19:13 +0530

    netlify setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:"wedding-invitation3",
+  base:"/",
   server: {
     host: "::",
     port: 8080,
